0c61593a2c550f0239084fa8c0c6201bc8ec9934
hungtq1016 <hungtq1016@gmail.com>
2022-10-28 23:35:37 +0700

    v1.1

--- src/Controller/Routing.php
+++ src/Controller/Routing.php
@@ -1,31 +1,55 @@
 <?php
-          $routes = [];
-          route('/', function()
-          {
-            require 'src/View/Top/index.php';
-          }) ;
+  $routes = [];
+  route('/', function (){
+      require 'src/View/TopView/index.php';
+  });
+  route('/books', function (){
+    require 'src/View/BookView/index.php';
+  });
+  route('/book', function (){
+    require 'src/View/BookView/add.php';
+  });
+  route('/book-view', function (){
+    require 'src/View/BookView/view.php';
+  });
+  route('/borrows', function (){
+    require 'src/View/BorrowView/index.php';
+  });
+  route('/borrow', function (){
+    require 'src/View/BorrowView/add.php';
+  });
+  route('/users', function (){
+    require 'src/View/UserView/index.php';
+  });
+  route('/user', function (){
+    require 'src/View/UserView/add.php';
+  });
 
-          function route(string $path, callable $callback) {
-            global $routes;
-            $routes[$path] = $callback;
-          }
-          
-          run();
-          
-          function run() {
-            global $routes;
-            $uri = $_SERVER['REQUEST_URI'];
-            $found = false;
-            foreach ($routes as $path => $callback) {
-              if ($path !== $uri) continue;
-          
-              $found = true;
-              $callback();
-            }
-          
-            if (!$found) {
-              $notFoundCallback = $routes['/404'];
-              $notFoundCallback();
-            }
-          }
-        ?>
+  function route(string $path, callable$callback)
+  {
+      global $routes;
+      $routes[$path] = $callback;
+  }
+
+  run();
+
+  function run()
+  {
+      global $routes;
+      $uri = $_SERVER['REQUEST_URI'];
+      $found = false;
+      foreach ($routes as $path => $callback)
+      {
+          if ($path !== $uri) continue;
+
+          $found = true;
+          $callback();
+      }
+
+      if (!$found)
+      {
+          $notFoundCallback = $routes['/404'];
+          $notFoundCallback();
+      }
+  }
+?>
---
+++ src/View/BorrowView/add.php
@@ -0,0 +1,78 @@
+<div class="page-header d-print-none">
+    <div class="container-xl">
+        <div class="row g-2 align-items-center">
+        <div class="col">
+                <!-- Page pre-title -->
+                <div class="page-pretitle">
+                    Quản Lý Sách
+                </div>
+                <h2 class="page-title">
+                    Thêm Sách
+                </h2>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="page-body">
+    <div class="container-sm">
+        <div class="row row-cards justify-content-center">
+            <div class="col-6">
+                <form action="https://httpbin.org/post" method="post" class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Thêm</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <div class="row">
+                                    <div class="col-md-6 col-xl-12">
+                                        <div class="mb-3">
+                                            <label class="form-label">Tên Sách</label>
+                                            <input type="text" class="form-control" name="bookname" placeholder="Nhập tên sách...">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Năm Phát Hành</label>
+                                            <input type="date" class="form-control" name="bookdate" default>
+                                        </div>
+                                        <div class="mb-3">
+                                            <div class="form-label">Tác Giả</div>
+                                            <select class="form-select" name="author">
+                                                <option value="1">Nguyễn Văn A</option>
+                                                <option value="2">Trần Văn B</option>
+                                                <option value="3">Lê Thị C</option>
+                                            </select>
+                                        </div>
+                                        <div class="mb-3">
+                                            <div class="form-label">Thể Loại</div>
+                                            <select class="form-select" name="category">
+                                                <option value="1">Hành Động</option>
+                                                <option value="2">Viễn Tưởng</option>
+                                                <option value="3">Ngôn Tình</option>
+                                            </select>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Số Lượng</label>
+                                            <input type="number" class="form-control" name="bookqty" value="1" min="0">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Tóm Tắt</label>
+                                            <textarea class="form-control" name="booksynopsis" rows="6" placeholder="Tóm tắt nội dung quyển sách.."></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer text-end">
+                        <div class="d-flex">
+                            <a href="/books" class="btn btn-link">Trở Về</a>
+                            <button type="submit" class="btn btn-primary ms-auto">Xác Nhận</button>
+                        </div>
+                    </div>
+                    </div>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
